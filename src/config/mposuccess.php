8eed69f7864fcdae4c1a5ed00ce08a84769eea30
Yan4ik <yan4ik345@yandex.by>
2015-08-25 23:58:55 +0300

    [#102010266] Fix Add dynamic title

--- src/config/mposuccess.php
+++ src/config/mposuccess.php
@@ -3,5 +3,16 @@
 return [
     'site_url' => '/',
     'profile_url' => 'profile',
-    'teacher_url' => 'admin'
+    'teacher_url' => 'admin',
+
+    /*
+     * Переменная $company_title - переменная которая всегда есть в заголовке в виде названия сайта или компании.
+     */
+    'company_title' => 'Test_Company',
+
+    /*
+     * Переменная $default_title - это переменная вызывается в случае отсутствия переменной заголовка.
+     */
+    'default_title' => 'Значение по умолчанию'
+
 ];
--- src/views/layouts/head.blade.php
+++ src/views/layouts/head.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8"/>
-<title>Metronic | Page Layouts - Search Box On Header 1</title>
+<title>{{ $title or config('mposuccess.default_title')}}: {{ config('mposuccess.company_title') }}</title>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
 <meta http-equiv="Content-type" content="text/html; charset=utf-8">
