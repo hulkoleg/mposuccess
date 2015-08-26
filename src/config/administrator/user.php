28fb36e66d2f9015ac773638de8e60b2c4e2fd35
nicklifs <usr@169.254.6.218>
2015-08-26 09:34:51 +0300

    [#101974090] test

--- src/config/administrator/user.php
+++ src/config/administrator/user.php
@@ -16,27 +16,50 @@ return array(
 	 * The display columns
 	 */
 	'columns' => array(
-		'id',
-		'name',
-		'email'
+		'id' 			=> array('title' => 'ID'),
+		'name' 			=> array('title' => 'Имя пользователя'),
+		'email' 		=> array('title' => 'Почта'),
+		'created_at'	=> array('title' => 'Создан'),
+		'updated_at'	=> array('title' => 'Изменён'),
+		'password'		=> array('title' => 'Хэш пароля')
 	),
 
 	/**
 	 * The filter set
 	 */
 	'filters' => array(
-		'id',
-		'name',
-		'email'
+		'id' 			=> array('title' => 'ID'),
+		'name' 			=> array('title' => 'Имя пользователя'),
+		'email'			=> array('title' => 'Почта'),
+		'created_at' 	=> array('title' => 'Создан', 'type' => 'datetime'),
+		'updated_at' 	=> array('title' => 'Изменён', 'type' => 'datetime'),
 	),
 
 	/**
 	 * The editable fields
 	 */
 	'edit_fields' => array(
-		'id',
-		'name',
-		'email'
+		'id' 		=> array('title' => 'ID'),
+		'name'		=> array('title' => 'Имя пользователя'),
+		'email'		=> array('title' => 'Почта'),
+		'password'	=> array('title' => 'Пароль')
 	),
+
+	/**
+	 * The validation rules for the form, based on the Laravel validation class
+	 *
+	 * @type array
+	 */
+	'rules' => array(
+		'name' => 'required|min:3|max:255',
+		'email' => 'required|email|max:255|unique:users',
+		'password' => 'required|min:6',
+	),
+
+	//test
+	'before_save' => function(&$data)
+	{
+		$data['name'] = $data['name'] . ' - test';
+	},
 
 );
