f74a20cce91820b47fec355580d22219363bf6ac
nicklifs <usr@169.254.6.218>
2015-08-28 20:56:59 +0300

    [#101974090] add seeder (add roles and users)

---
+++ src/seeds/UserSeeder.php
@@ -0,0 +1,75 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Bican\Roles\Models\Role;
+use Notprometey\Mposuccess\Models\User;
+
+class UserSeeder extends Seeder {
+
+    public function run()
+    {
+        /*
+         * Create user and role ADMIN
+         */
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@mposuccess.ru',
+            'password' => bcrypt('admin'),
+        ]);
+
+        $adminRole = Role::create([
+            'name' => 'Admin',
+            'slug' => 'admin',
+        ]);
+
+        $user->attachRole($adminRole);
+
+        /*
+         * Create user and role MODERATOR
+         */
+        $user = User::create([
+            'name' => 'moderator',
+            'email' => 'moderator@mposuccess.ru',
+            'password' => bcrypt('moderator'),
+        ]);
+
+        $moderatorRole = Role::create([
+            'name' => 'Moderator',
+            'slug' => 'moderator',
+        ]);
+
+        $user->attachRole($moderatorRole);
+
+        /*
+         * Create user and role USER
+         */
+        $user = User::create([
+            'name' => 'user',
+            'email' => 'user@mposuccess.ru',
+            'password' => bcrypt('user'),
+        ]);
+
+        $userRole = Role::create([
+            'name' => 'User',
+            'slug' => 'user',
+        ]);
+
+        $user->attachRole($userRole);
+
+        /*
+         * Create user and role badUser - not verified user
+         */
+        $user = User::create([
+            'name' => 'test',
+            'email' => 'test@mposuccess.ru',
+            'password' => bcrypt('test'),
+        ]);
+
+        $badUserRole = Role::create([
+            'name' => 'badUser',
+            'slug' => 'bad.user',
+        ]);
+
+        $user->attachRole($badUserRole);
+    }
+}
